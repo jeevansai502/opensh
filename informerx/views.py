510f6c41f9bae81fffdae1ac200575193bdd146d
jeevansai <jeevansai@linux-tjt9.suse>
2018-06-29 15:50:46 +0530

    first commit

--- informerx/views.py
+++ informerx/views.py
@@ -3,7 +3,6 @@ import os
 from django.conf import settings
 from django.http import HttpResponse
 
-from .models import PageView
 # Create your views here.
 def health(request):
     return HttpResponse(1)

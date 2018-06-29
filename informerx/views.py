7ca37ec5030861f758c987cede058bb94003aea4
jeevansai <jeevansai@linux-tjt9.suse>
2018-06-29 15:38:50 +0530

    first commit

--- informerx/views.py
+++ informerx/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def health(request):
+    return HttpResponse(PageView.objects.count())
--- portal/urls.py
+++ portal/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from informerx.views import health
 
 urlpatterns = [
+    url(r'^health$', health),
     path('admin/', admin.site.urls),
 ]
